f417fbade60a132f37c5fe83e98792039579525d
HernandoMV <hernybiotec@gmail.com>
2025-10-16 15:27:39 +0200

    .

--- weight_excel_convertor.py
+++ weight_excel_convertor.py
@@ -9,11 +9,11 @@ from datetime import datetime
 #%%
 
 # Load the ODS file
-file_path = "/mnt/c/Users/HMARTINEZ/Downloads/Mice and Citric Acid Weight Measurements (18.08.2025).xlsx"
+file_path = "/mnt/c/Users/HMARTINEZ/Downloads/Mice and Citric Acid Weight Measurements (23.09.2025).xlsx"
 
 # Use the third row (index 2) as header
-df_clean = pd.read_excel(file_path, engine='openpyxl', sheet_name="Mice weights", skiprows=3, nrows=12)
-df_ca = pd.read_excel(file_path, engine='openpyxl', sheet_name="Citric acid weight", skiprows=3, nrows=2)
+# df_clean = pd.read_excel(file_path, engine='openpyxl', sheet_name="Mice weights", skiprows=3, nrows=12)
+df_ca = pd.read_excel(file_path, engine='openpyxl', sheet_name="Citric acid weight", skiprows=3, nrows=7)
 
 #%%
 # Extract the original column names for processing
@@ -72,6 +72,7 @@ def convert_date_format(col_name):
         return col_name  # Non-date columns remain as they are
     except Exception:
         return "removeMe"
+# %%
 # Apply the date conversion to all column names
 df_clean.columns = [convert_date_format(col) for col in df_clean.columns]
 # Remove columns that were marked for removal
@@ -95,8 +96,37 @@ print(df_clean)
 df_clean.to_pickle("cleaned_weight_measurements.pkl")
 
 # %%
-# combine both rows of the citric acid weight DataFrame into a single row
-df_ca_combined = df_ca.iloc[0].combine_first(df_ca.iloc[1])
+# # combine both rows of the citric acid weight DataFrame into a single row
+# df_ca_combined = df_ca.iloc[0].combine_first(df_ca.iloc[1])
+# df_ca_combined = pd.DataFrame([df_ca_combined])
+# # drop first column
+# df_ca_combined = df_ca_combined.drop(columns=df_ca_combined.columns[0])
+# #make the column names dates
+# df_ca_combined.columns = [convert_date_format(col) for col in df_ca_combined.columns]
+# # Reset the index
+# df_ca_combined.reset_index(drop=True, inplace=True)
+# # first entry to 600
+# df_ca_combined.iloc[0, 0] = 600
+# # make the 4th of august a nan value as it has a comment
+# df_ca_combined['2025-08-04'] = np.nan
+# # remove the data between 2025-07-14 and 2025-07-21 (inclusive), as scale malfunctioned
+# df_ca_combined = df_ca_combined.loc[:, ~df_ca_combined.columns.isin(
+#     ['2025-07-14', '2025-07-15', '2025-07-16', '2025-07-17', '2025-07-18', '2025-07-19', '2025-07-20', '2025-07-21']
+# )]
+# # drop nans
+# df_ca_combined = df_ca_combined.dropna(axis=1, how='all')
+
+# # turn this into consumption of CA by getting the diff
+# df_ca_combined = df_ca_combined.diff(axis=1).fillna(df_ca_combined.iloc[:, 0])
+# # make the entry of 2025-07-01 a nan value as it was change of bottle
+# change_of_bottle_dates = ['2025-07-01', '2025-07-22', '2025-07-28', '2025-08-05', '2025-08-13']
+# for date in change_of_bottle_dates:
+#     if date in df_ca_combined.columns:
+#         df_ca_combined[date] = np.nan
+
+# %%
+# get only the difference row
+df_ca_combined = df_ca.iloc[5]
 df_ca_combined = pd.DataFrame([df_ca_combined])
 # drop first column
 df_ca_combined = df_ca_combined.drop(columns=df_ca_combined.columns[0])
@@ -104,27 +134,9 @@ df_ca_combined = df_ca_combined.drop(columns=df_ca_combined.columns[0])
 df_ca_combined.columns = [convert_date_format(col) for col in df_ca_combined.columns]
 # Reset the index
 df_ca_combined.reset_index(drop=True, inplace=True)
-# first entry to 600
-df_ca_combined.iloc[0, 0] = 600
-# make the 4th of august a nan value as it has a comment
-df_ca_combined['2025-08-04'] = np.nan
-# remove the data between 2025-07-14 and 2025-07-21 (inclusive), as scale malfunctioned
-df_ca_combined = df_ca_combined.loc[:, ~df_ca_combined.columns.isin(
-    ['2025-07-14', '2025-07-15', '2025-07-16', '2025-07-17', '2025-07-18', '2025-07-19', '2025-07-20', '2025-07-21']
-)]
-# drop nans
 df_ca_combined = df_ca_combined.dropna(axis=1, how='all')
-
-# turn this into consumption of CA by getting the diff
-df_ca_combined = df_ca_combined.diff(axis=1).fillna(df_ca_combined.iloc[:, 0])
-# make the entry of 2025-07-01 a nan value as it was change of bottle
-change_of_bottle_dates = ['2025-07-01', '2025-07-22', '2025-07-28', '2025-08-05', '2025-08-13']
-for date in change_of_bottle_dates:
-    if date in df_ca_combined.columns:
-        df_ca_combined[date] = np.nan
-
-
-
+# make the data float
+df_ca_combined = df_ca_combined.astype(float)
 # Show the combined DataFrame
 print(df_ca_combined)
 # %%
